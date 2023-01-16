64e99632372eed52f6c8cc25e92560332a4869f9
Bartlomiej Snochowski <bartlomiej.snochowski@gmail.com>
2023-01-16 02:10:56 +0100

    removed file and created new one in diffrent lociation

---
+++ src/currencies.js
@@ -0,0 +1,12 @@
+const currencies =
+[
+  { name: "EUR", rate: 4.65, valueName: "Euro" },
+  { name: "USD", rate: 4.38, valueName: "Dolar amerykański" },
+  { name: "GPB", rate: 5.34, valueName: "Funt brytyjski" },
+  { name: "CHF", rate: 4.73, valueName: "Frank szwajcarski" },
+  { name: "NOK", rate: 0.44, valueName: "Korona norweska" }
+];
+
+export default currencies;
+
+console.log(currencies);
