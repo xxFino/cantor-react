d1a0cb380a290370d0df8c759122f6929c5f7d34
Bartlomiej Snochowski <bartlomiej.snochowski@gmail.com>
2023-02-08 14:05:29 +0100

    centering footer

--- src/Footer/styled.js
+++ src/Footer/styled.js
@@ -2,5 +2,7 @@ import styled from "styled-components"
 
 export const FooterStyle = styled.footer`
     text-decoration: underline;
-    margin: 20px;
+    padding-top: 20px;
+    margin: 0 auto;    
+    width: 630px;
 `;
